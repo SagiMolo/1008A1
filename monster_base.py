0aa3845d428f9d9034db6a17a6fae50a29efe00c
SagiMolo <140663278+SagiMolo@users.noreply.github.com>
2023-08-02 14:25:47 +1000

    Task 1 progress

--- monster_base.py
+++ monster_base.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 import abc
 
+import stats
 from stats import Stats
 
 class MonsterBase(abc.ABC):
@@ -11,44 +12,50 @@ class MonsterBase(abc.ABC):
 
         :simple_mode: Whether to use the simple or complex stats of this monster
         :level: The starting level of this monster. Defaults to 1.
+        :original_level: stores the level of the monster when it was created for evolution purposes.
         """
-        raise NotImplementedError
+        self.original_level = level
+        self.level = level
+        if simple_mode:
+            self.get_simple_stats()
 
     def get_level(self):
         """The current level of this monster instance"""
-        raise NotImplementedError
+        return self.level
 
     def level_up(self):
         """Increase the level of this monster instance by 1"""
-        raise NotImplementedError
+        self.level += 1
 
     def get_hp(self):
         """Get the current HP of this monster instance"""
-        raise NotImplementedError
+        return self.hp()
 
     def set_hp(self, val):
         """Set the current HP of this monster instance"""
-        raise NotImplementedError
+        self.hp = val
 
     def get_attack(self):
         """Get the attack of this monster instance"""
-        raise NotImplementedError
+        return self.attack()
 
     def get_defense(self):
         """Get the defense of this monster instance"""
-        raise NotImplementedError
+        return self.defense()
 
     def get_speed(self):
         """Get the speed of this monster instance"""
-        raise NotImplementedError
+        return self.speed()
 
     def get_max_hp(self):
         """Get the maximum HP of this monster instance"""
-        raise NotImplementedError
+        return self.max_hp()
 
     def alive(self) -> bool:
         """Whether the current monster instance is alive (HP > 0 )"""
-        raise NotImplementedError
+        if self.get_hp() > 0:
+            return True
+        return False
 
     def attack(self, other: MonsterBase):
         """Attack another monster instance"""
@@ -58,13 +65,23 @@ class MonsterBase(abc.ABC):
         # Step 4: Lose HP
         raise NotImplementedError
 
+    def get_original_level(self) -> int:
+        """Returns the level of the monster when it was just created"""
+        return self.original_level()
+
     def ready_to_evolve(self) -> bool:
         """Whether this monster is ready to evolve. See assignment spec for specific logic."""
-        raise NotImplementedError
+        if self.get_evolution() != None and self.get_level() > self.get_original_level():
+            return True
+        return False
 
     def evolve(self) -> MonsterBase:
         """Evolve this monster instance by returning a new instance of a monster class."""
-        raise NotImplementedError
+        return self.get_evolution()(True, self.get_level())
+
+    def __str__(self):
+        """Returns string value of the monster"""
+        return f"LV.{self.get_level()} {self.get_name()}, {self.get_hp()}/{self.get_max_hp()} HP"
 
     ### NOTE
     # Below is provided by the factory - classmethods
